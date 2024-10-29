a2a51292ec84c2ad20163a46d57dc5c78e178a26
Arcdarelle <darelle.ghomo@utrains.com>
2024-10-29 13:17:33 +0100

    update files

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "region" {
 variable "bucket_name" {
     type = string
     description = "The name of the your bucket"
-    default = "buckethiring" # replace here by the name of your bucket  
+    default = "buckethirings" # replace here by the name of your bucket  
 }
 
 variable "cp-path" {
